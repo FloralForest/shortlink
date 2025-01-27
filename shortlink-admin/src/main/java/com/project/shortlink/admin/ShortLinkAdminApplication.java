8dfc5b5985094190a03469618780eb9831852043
kaii <3450922558@qq.com>
2025-01-27 22:10:54 +0800

    create

--- shortlink-admin/src/main/java/com/project/shortlink/admin/ShortLinkAdminApplication.java
+++ shortlink-admin/src/main/java/com/project/shortlink/admin/ShortLinkAdminApplication.java
@@ -1,9 +1,12 @@
 package com.project.shortlink.admin;
 
+import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+//持久层接口扫描器
+@MapperScan("com.project.shortlink.admin.dao.mapper")
 public class ShortLinkAdminApplication {
     public static void main(String[] args) {
         SpringApplication.run(ShortLinkAdminApplication.class, args);
