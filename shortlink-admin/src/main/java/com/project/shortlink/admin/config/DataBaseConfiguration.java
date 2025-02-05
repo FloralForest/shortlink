7a6dc7c7b599087f1d5c6a1306c9a24c104d8b66
kaii <3450922558@qq.com>
2025-02-05 22:00:29 +0800

    create

---
+++ shortlink-admin/src/main/java/com/project/shortlink/admin/config/DataBaseConfiguration.java
@@ -0,0 +1,18 @@
+package com.project.shortlink.admin.config;
+
+import com.baomidou.mybatisplus.annotation.DbType;
+import com.baomidou.mybatisplus.extension.plugins.MybatisPlusInterceptor;
+import com.baomidou.mybatisplus.extension.plugins.inner.PaginationInnerInterceptor;
+import org.springframework.context.annotation.Bean;
+
+/**
+ * mybatis 分页插件
+ */
+public class DataBaseConfiguration {
+    @Bean
+    public MybatisPlusInterceptor mybatisPlusInterceptor(){
+        MybatisPlusInterceptor interceptor = new MybatisPlusInterceptor();
+        interceptor.addInnerInterceptor(new PaginationInnerInterceptor(DbType.MYSQL));
+        return interceptor;
+    }
+}
---
+++ shortlink-project/src/main/java/com/project/shortlink/project/dto/req/LinkPageDTO.java
@@ -0,0 +1,17 @@
+package com.project.shortlink.project.dto.req;
+
+import com.baomidou.mybatisplus.extension.plugins.pagination.Page;
+import com.project.shortlink.project.dao.entity.TLink;
+import lombok.Data;
+
+/**
+ * 短链接分页请求参数
+ */
+@Data
+public class LinkPageDTO extends Page<TLink> {
+
+    /**
+     * 分组标识，获得该组下的短链接
+     */
+    private String gid;
+}
