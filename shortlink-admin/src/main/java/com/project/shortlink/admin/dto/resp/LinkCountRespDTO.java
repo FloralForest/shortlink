a68191be8b66769d520a73135bf2c110d373a8c5
kaii <3450922558@qq.com>
2025-02-06 02:34:52 +0800

    create

---
+++ shortlink-admin/src/main/java/com/project/shortlink/admin/dto/resp/LinkCountRespDTO.java
@@ -0,0 +1,19 @@
+package com.project.shortlink.admin.dto.resp;
+
+import lombok.Data;
+
+/**
+ * 短链接数量
+ */
+@Data
+public class LinkCountRespDTO {
+    /**
+     * 分组标识
+     */
+    private String gid;
+    /**
+     * 短链接数量
+     */
+    private Integer linkCount;
+}
+
