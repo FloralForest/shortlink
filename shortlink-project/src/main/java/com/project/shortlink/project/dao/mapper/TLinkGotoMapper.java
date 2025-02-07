b4dc45b1faef9dc7e7204251b66a5b3af4d47e38
kaii <3450922558@qq.com>
2025-02-08 02:07:23 +0800

    create

---
+++ shortlink-project/src/main/java/com/project/shortlink/project/dao/mapper/TLinkGotoMapper.java
@@ -0,0 +1,18 @@
+package com.project.shortlink.project.dao.mapper;
+
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+import com.project.shortlink.project.dao.entity.TLinkGoto;
+import org.apache.ibatis.annotations.Mapper;
+
+/**
+ * <p>
+ *  Mapper 接口
+ * </p>
+ *
+ * @author project
+ * @since 2025-02-02
+ */
+@Mapper
+public interface TLinkGotoMapper extends BaseMapper<TLinkGoto> {
+
+}
