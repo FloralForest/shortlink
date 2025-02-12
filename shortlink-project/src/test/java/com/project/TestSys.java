0d07ba16ff117619efe82e31a1a3cee07dbef8ea
kaii <3450922558@qq.com>
2025-02-12 20:33:29 +0800

    create

--- shortlink-project/src/test/java/com/project/TestSys.java
+++ shortlink-project/src/test/java/com/project/TestSys.java
@@ -1,10 +1,11 @@
 package com.project;
 
 import cn.hutool.core.util.StrUtil;
+import org.junit.jupiter.api.Test;
 
 public class TestSys {
 
-    @org.junit.jupiter.api.Test
+    @Test
     public void test(){
         String furl = "baidu.com/3V21X8";
         System.out.println(StrUtil.sub(furl, furl.indexOf("/"), furl.length()));
