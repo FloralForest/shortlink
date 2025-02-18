92f1ada1b3484727894e69ebdd2112c87395f592
kaii <3450922558@qq.com>
2025-02-18 16:09:26 +0800

    create

---
+++ shortlink-admin/src/main/java/com/project/shortlink/admin/config/UserFlowRiskControlConfiguration.java
@@ -0,0 +1,30 @@
+package com.project.shortlink.admin.config;
+
+import lombok.Data;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.stereotype.Component;
+
+/**
+ * 用户操作流量风控（限流）配置文件
+ */
+@Data
+@Component
+//注解获取yml配置
+@ConfigurationProperties(prefix = "short-link.flow-limit")
+public class UserFlowRiskControlConfiguration {
+
+    /**
+     * 是否开启用户流量风控验证
+     */
+    private Boolean enable;
+
+    /**
+     * 流量风控时间窗口，单位：秒
+     */
+    private String timeWindow;
+
+    /**
+     * 流量风控时间窗口内可访问次数
+     */
+    private Long maxAccessCount;
+}
---
+++ shortlink-admin/src/main/resources/lua/user_flow_risk_control.lua
@@ -0,0 +1,17 @@
+-- 用户操作流量风控（限流）lua脚本
+-- 设置用户访问频率限制的参数
+local username = KEYS[1]
+local timeWindow = tonumber(ARGV[1]) -- 时间窗口，单位：秒
+
+-- 构造 Redis 中存储用户访问次数的键名
+local accessKey = "short-link:user-flow-risk-control:" .. username
+
+-- 原子递增访问次数，并获取递增后的值
+local currentAccessCount = redis.call("INCR", accessKey)
+
+-- 设置键的过期时间 若键已存在，原子递增访问次数并返回递增后的值
+if currentAccessCount == 1 then
+    redis.call("EXPIRE", accessKey, timeWindow)
+end
+-- 返回当前访问次数
+return currentAccessCount
